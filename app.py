f4a102d6720db75e3ec481395f80ac77fa1e092c
Stella-Long <huierl@nycstudents.net>
2024-09-20 10:08:20 -0400

    Starting challenge #1

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
-x = 3
-y = float(3)
-print(x,y)
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
-print(values[0])
-print(values[6])
-"test"
-["t","e","s","t"]
-x = "this is a thing"
-y = x.split( )
-z = y[0]
-print(y)
-print(z)
+""" Integer & Float """
+# x = 3
+# y = float(3)
+# print(x,y)
+""" List """
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+# print(values[0])
+# print(values[6])
+""" String """
+# "test"
+# ["t","e","s","t"]
+""" Booleans """
+# x = "this is a thing"
+# y = x.split( )
+# z = y[0]
+# print(y)
+# print(z)
+# day_of_week = input("what day is it? ")
+# if day_of_week == "Friday":
+#     print("correct")
+# else:
+#     print("incorrect")
+# x = "test"
+# print(f"hello {x}")
+# temp = 75
+# if temp > 68:
+#     print('warm')
+# elif temp == 68:
+#     print('perfect')
+# else:
+#     print('cold')
+x = 16
+y = 7
+if x is 2 ==0:
+    (print: both are even)
+if x is 2 ==0:
+    (print: atleast one is even )
+if x is 2 ==0:
+    (print: neither are even)
